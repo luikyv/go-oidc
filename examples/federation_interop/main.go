077789a1761f1431dc5bd57e5eb3f1b3cfb46ea9
luikymagno <luikymagno@gmail.com>
2025-04-23 16:57:50 -0300

    wip: federation

--- examples/federation_interop/main.go
+++ examples/federation_interop/main.go
@@ -65,8 +65,8 @@ func main() {
 
 	templatesDirPath := filepath.Join(workingDir, "../templates")
 	jwksFilePath := filepath.Join(workingDir, "../keys/server.jwks")
-	serverCertFilePath := filepath.Join(workingDir, "../keys/server.cert")
-	serverCertKeyFilePath := filepath.Join(workingDir, "../keys/server.key")
+	serverCertFilePath := filepath.Join(workingDir, "../keys/7GIzGiS15jp-WVQLbwv9KKdUY5gRSREI4J8yvBMCZA0.pem")
+	serverCertKeyFilePath := filepath.Join(workingDir, "../keys/6a81a0b2-a6f7-479d-9f8e-76add4dd629f-resource_server_signing.key")
 	clientJWKSFilePath := filepath.Join(workingDir, "../keys/client_one.jwks")
 
 	// Set up federation JWKS's.
