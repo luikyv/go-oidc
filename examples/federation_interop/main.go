0e00965d1c24ca57f5a46c1bed19c6e6fb0620dd
luikymagno <luikymagno@gmail.com>
2025-04-28 08:16:44 -0300

    wip: federation

--- examples/federation_interop/main.go
+++ examples/federation_interop/main.go
@@ -2,18 +2,13 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"net/http"
-	"net/url"
 	"path/filepath"
 	"runtime"
 
-	"github.com/go-jose/go-jose/v4"
 	"github.com/go-jose/go-jose/v4/json"
-	"github.com/go-jose/go-jose/v4/jwt"
 	"github.com/luikyv/go-oidc/examples/authutil"
-	"github.com/luikyv/go-oidc/internal/timeutil"
 	"github.com/luikyv/go-oidc/pkg/goidc"
 	"github.com/luikyv/go-oidc/pkg/provider"
 )
@@ -21,7 +16,7 @@ import (
 const (
 	TrustAnchorFedID = "https://federation.pr-2721.ci.raidiam.io/federation_entity/5d0acc74-a8ea-4f9e-8238-3b4b22166e9a"
 
-	OPFedID = "https://ec2-50-19-156-19.compute-1.amazonaws.com"
+	OPFedID = "https://oidfed.luikyv.com"
 	// OPFedID   = "https://auth.localhost"
 	OPFedJWKS = `
 		{
@@ -42,24 +37,6 @@ const (
 			]
 		}
 	`
-
-	ClientFedID   = "https://50.19.156.19"
-	ClientFedJWKS = `
-		{
-			"keys": [
-				{
-					"kty": "EC",
-					"d": "KiNS6fScsAuphhFQbPKw5YjXu1Nvz7gi47at_f8KSow",
-					"use": "sig",
-					"crv": "P-256",
-					"kid": "cW6BPniOmyVMvwIlTlh2SanDl3KmferQuT8LS7CIB9U",
-					"x": "1Mlnn7kLJJgWELLdF-pqw2j6CGYRY0lJbu8m4i-esdM",
-					"y": "QCezZwp5knqiRivSjDNPfPNHR79KAvFQbPcE4rKvnLo",
-					"alg": "ES256"
-				}
-			]
-		}
-	`
 )
 
 func main() {
@@ -69,23 +46,10 @@ func main() {
 
 	templatesDirPath := filepath.Join(workingDir, "../templates")
 	jwksFilePath := filepath.Join(workingDir, "../keys/server.jwks")
-	serverCertFilePath := filepath.Join(workingDir, "../keys/server.crt")
-	serverCertKeyFilePath := filepath.Join(workingDir, "../keys/server.key")
-	clientJWKSFilePath := filepath.Join(workingDir, "../keys/client_one.jwks")
 
 	// Set up federation JWKS's.
 	var opFedJWKS goidc.JSONWebKeySet
 	_ = json.Unmarshal([]byte(OPFedJWKS), &opFedJWKS)
-
-	var clientFedJWKS goidc.JSONWebKeySet
-	_ = json.Unmarshal([]byte(ClientFedJWKS), &clientFedJWKS)
-
-	// Set up federation URL's.
-	opFedURL, _ := url.Parse(OPFedID)
-	clientFedURL, _ := url.Parse(ClientFedID)
-
-	// Create and configure the openid provider and a client.
-	client, clientJWKS := authutil.ClientPrivateKeyJWT(ClientFedID, clientJWKSFilePath)
 
 	op, err := provider.New(
 		goidc.ProfileOpenID,
@@ -123,61 +87,7 @@ func main() {
 	}
 
 	// Set up the server.
-	mux := http.NewServeMux()
-
-	mux.Handle(opFedURL.Hostname()+"/", op.Handler())
-
-	mux.HandleFunc("GET "+clientFedURL.Hostname()+"/.well-known/openid-federation", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/entity-statement+jwt")
-		w.WriteHeader(http.StatusOK)
-
-		claims := map[string]any{
-			"iss": ClientFedID,
-			"sub": ClientFedID,
-			"iat": timeutil.TimestampNow(),
-			"exp": timeutil.TimestampNow() + 600,
-			"metadata": map[string]any{
-				"openid_relying_party": client.ClientMeta,
-			},
-			"jwks":            clientFedJWKS.Public(),
-			"authority_hints": []string{TrustAnchorFedID},
-		}
-
-		opts := (&jose.SignerOptions{}).WithHeader("typ", "entity-statement+jwt")
-		jwk := clientFedJWKS.Keys[0]
-		signer, _ := jose.NewSigner(jose.SigningKey{
-			Algorithm: goidc.SignatureAlgorithm(jwk.Algorithm),
-			Key:       jwk,
-		}, opts)
-		jws, _ := jwt.Signed(signer).Claims(claims).Serialize()
-
-		_, _ = w.Write([]byte(jws))
-	})
-
-	log.Println(authReqURL(clientJWKS))
-	if err := http.ListenAndServeTLS(authutil.Port, serverCertFilePath, serverCertKeyFilePath, mux); err != nil && err != http.ErrServerClosed {
+	if err := op.Run(":80"); err != nil && err != http.ErrServerClosed {
 		log.Fatal(err)
 	}
-}
-
-func authReqURL(clientJWKS goidc.JSONWebKeySet) string {
-	jarOpts := (&jose.SignerOptions{}).WithHeader("typ", "JWT")
-	jwk := clientJWKS.Keys[0]
-	signer, _ := jose.NewSigner(jose.SigningKey{
-		Algorithm: goidc.SignatureAlgorithm(jwk.Algorithm),
-		Key:       jwk,
-	}, jarOpts)
-	jar, _ := jwt.Signed(signer).Claims(map[string]any{
-		"iss":           ClientFedID,
-		"aud":           OPFedID,
-		"iat":           timeutil.TimestampNow(),
-		"exp":           timeutil.TimestampNow() + 600,
-		"client_id":     ClientFedID,
-		"redirect_uri":  "http://localhost/callback",
-		"scope":         "openid",
-		"response_type": "code id_token",
-		"nonce":         "random_nonce",
-	}).Serialize()
-
-	return fmt.Sprintf("%s/authorize?client_id=%s&response_type=code id_token&scope=openid&request=%s\n", OPFedID, ClientFedID, jar)
 }
